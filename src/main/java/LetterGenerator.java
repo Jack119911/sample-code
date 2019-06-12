9a2aed0b1cf9f56dd42be8940e5714081381b842
jakob.schoenicke@arcor.de <gitterino15>
2019-06-12 13:56:11 +0200

    Initial commit

---
+++ src/main/java/LetterGenerator.java
@@ -0,0 +1,90 @@
+import java.util.Random;
+
+public class LetterGenerator {
+
+    private String currentLetter;
+    private String[] questions = {
+            "Wie steht es um Ihre Gesundheit?",
+            "Haben Sie das Fu\u00dfballspiel gestern Abend gesehen?",
+            "Wie geht es der Familie?",
+            "Kennen Sie schon den neuen Film mit Leonardo DiCaprio",
+            "Wie ist das Wetter bei Ihnen?"
+    };
+    private String[] statements = {
+            "Meine Frau hat gestern einen fantastischen Apfelkuchen gebacken.",
+            "Beim Wandern gestern konnte ich mich sehr entspannen.",
+            "Der neue Schirach ist ein wahres Meisterwerk.",
+            "Die letzten Jahre habe ich den Jazz f\u00fcr mich entdeckt.",
+            "Im n\u00e4chsten Sommer machen wir Urlaub in Island"
+    };
+
+
+    public LetterGenerator() {
+
+    }
+
+    public String generateLetter(String sirName, boolean isMale) {
+        currentLetter = "";
+        generateGreeting(sirName, isMale);
+        generateBody();
+        generateRegards();
+        return currentLetter;
+    }
+
+    private void generateGreeting(String sirName, boolean isMale) {
+        if (isMale) {
+            addLineToLetter("Sehr geehrter Herr " + sirName);
+        } else {
+            addLineToLetter("Sehr geehrte Frau " + sirName);
+        }
+        addNewLine();
+    }
+
+    private void generateBody() {
+        for(int i = 0; i < 5; i++) {
+            generateSentence();
+        }
+    }
+
+    private void generateSentence() {
+        if (Math.random() < 0.6) {
+            generateStatement();
+        } else {
+            generateQuestion();
+        }
+    }
+
+    private void generateQuestion() {
+        String randomQuestion = getRandomEntry(questions);
+        addLineToLetter(randomQuestion);
+    }
+
+    private void generateStatement() {
+        String randomStatement = getRandomEntry(statements);
+        addLineToLetter(randomStatement);
+    }
+
+    private String getRandomEntry(String[] possibilities) {
+        int randomIndex = new Random().nextInt(possibilities.length);
+        return possibilities[randomIndex];
+    }
+
+    private void generateRegards() {
+        addNewLine();
+        addLineToLetter("Viele Gr\u00fc\u00dfe,");
+    }
+
+    private void addLineToLetter(String line) {
+        addToLetter(line);
+        addNewLine();
+    }
+
+    private void addNewLine() {
+        addToLetter("\n");
+    }
+
+    private void addToLetter(String toAdd) {
+        currentLetter += toAdd;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        LetterGenerator letterGenerator = new LetterGenerator();
+        System.out.println(letterGenerator.generateLetter("Schoenicke", true));
+    }
+
+}
